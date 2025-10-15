e1ec165874e13ea7f0b2bb96c5a1354512b322be
Adrian Cano Saiz <acano-sa@c2r6s1.42madrid.com>
2025-10-15 18:16:25 +0200

    corregidos errores y norminette (no bonus todavía)

--- get_next_line.c
+++ get_next_line.c
@@ -1,66 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acano-sa <acano-sa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 18:08:41 by acano-sa          #+#    #+#             */
+/*   Updated: 2025/10/15 18:15:07 by acano-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *remainder;
-    char buffer[BUFFER_SIZE + 1];
-    char *line;
+	static char	*remainder;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*line;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-
-    remainder = read_and_accumulate(fd, remainder, buffer);
-    if (!remainder)
-        return (NULL);
-
-    line = extract_line(remainder);
-    remainder = save_remainder(remainder);
-
-    return (line);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	remainder = read_and_accumulate(fd, remainder, buffer);
+	if (!remainder)
+		return (NULL);
+	if (remainder[0] == '\0')
+	{
+		free(remainder);
+		remainder = NULL;
+		return (NULL);
+	}
+	line = extract_line(remainder);
+	remainder = save_remainder(remainder);
+	return (line);
 }
 
-char *read_and_accumulate(int fd, char *remainder, char *buffer)
+char	*read_and_accumulate(int fd, char *remainder, char *buffer)
 {
-    int bytes_read;
+	int	bytes_read;
 
-    while (!ft_strchr(remainder, '\n') &&
-           (bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
-    {
-        buffer[bytes_read] = '\0';
-        remainder = ft_strjoin(remainder, buffer);
-    }
-    if (bytes_read < 0)
-        return (NULL);
-    return (remainder);
+	bytes_read = 1;
+	while (!ft_strchr(remainder, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(remainder);
+			return (NULL);
+		}
+		if (bytes_read == 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		remainder = ft_strjoin(remainder, buffer);
+		if (!remainder)
+			return (NULL);
+	}
+	return (remainder);
 }
 
-char *extract_line(char *remainder)
+char	*extract_line(char *remainder)
 {
-    int i = 0;
-    char *line;
+	int		i;
+	char	*line;
 
-    while (remainder[i] && remainder[i] != '\n')
-        i++;
-    if (remainder[i] == '\n')
-        line = ft_substr(remainder, 0, i + 1);
-    else
-        line = ft_substr(remainder, 0, i);
-    return (line);
+	if (!remainder)
+		return (NULL);
+	i = 0;
+	while (remainder[i] && remainder[i] != '\n')
+		i++;
+	if (remainder[i] == '\n')
+		line = ft_substr(remainder, 0, i + 1);
+	else
+		line = ft_substr(remainder, 0, i);
+	return (line);
 }
 
-char *save_remainder(char *remainder)
+char	*save_remainder(char *remainder)
 {
-    int i = 0;
-    char *tmp;
-    char *new_rem;
+	int		i;
+	char	*tmp;
+	char	*new_rem;
 
-    while (remainder[i] && remainder[i] != '\n')
-        i++;
-    tmp = remainder;
-    if (remainder[i] == '\n')
-        new_rem = ft_substr(remainder, i + 1, ft_strlen(remainder) - (i + 1));
-    else
-        new_rem = NULL;
-    free(tmp);
-    return (new_rem);
+	if (!remainder)
+		return (NULL);
+	i = 0;
+	while (remainder[i] && remainder[i] != '\n')
+		i++;
+	tmp = remainder;
+	if (remainder[i] == '\n')
+		new_rem = ft_substr(remainder, i + 1, ft_strlen(remainder) - (i + 1));
+	else
+		new_rem = NULL;
+	free(tmp);
+	return (new_rem);
 }
